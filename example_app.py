a063b77860fae486ed091fa1a7466ebca8be52dc
pandyandy <144467422+pandyandy@users.noreply.github.com>
2024-06-13 15:43:00 +0200

    Update

--- example_app.py
+++ example_app.py
@@ -15,5 +15,29 @@ kst.set_dev_mockup_headers({
 })
 
 kst.auth_check(REQUIRED_ROLE_ID)
-if st.button('create event'):
-    kst.create_event(123, {'data':'of_event'})
+
+if st.button('Create Event'):
+    kst.create_event()
+
+table_id = st.text_input("Table ID")
+
+submitted_get = st.button("Submit")
+if submitted_get:
+    df = kst.get_data(table_id)
+    if not df.empty: 
+        st.dataframe(df,use_container_width=True)
+        st.session_state['data'] = df
+    else:
+        st.error("No data retrieved for the given Table ID.")
+
+submitted_load = st.button("Load")
+if submitted_load:
+    if 'data' in st.session_state and not st.session_state['data'].empty:
+        kst.load_data(table_id, st.session_state['data'])
+else:
+    st.error("No data available to load.")
+
+st.session_state['uploaded_file'] = kst.add_keboola_table_selection()
+
+if not st.session_state['uploaded_file'].empty:
+    st.dataframe(st.session_state['uploaded_file'], use_container_width=True)
